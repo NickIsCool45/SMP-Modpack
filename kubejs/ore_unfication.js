56884225db3f536df99cfff126ba243cf97358b0
NickIsCool45 <72705077+NickIsCool45@users.noreply.github.com>
2021-12-04 18:44:53 -0800

    Add files via upload

---
+++ kubejs/ore_unfication.js
@@ -0,0 +1,412 @@
+// priority: 0
+
+onEvent('jei.hide.items', event => {
+	//Iron dust
+	event.hide('immersiveengineering:dust_iron')
+	event.hide('mekanism:dust_iron')
+	event.hide('pedestals:dustiron')
+	event.hide('appliedenergistics2:iron_dust')
+	//Iron gear
+	event.hide('titanium:iron_gear')
+	//Iron plate
+	event.hide('immersiveengineering:plate_iron')
+	event.hide('create:iron_sheet')
+	//Iron rod
+	event.hide('immersiveengineering:stick_iron')
+	event.hide('buildersaddition:iron_rod')
+
+	//Gold dust
+	event.hide('immersiveengineering:dust_gold')
+	event.hide('mekanism:dust_gold')
+	event.hide('pedestals:dustgold')
+	event.hide('appliedenergistics2:gold_dust')
+	//Gold gear
+	event.hide('titanium:gold_gear')
+	//Gold Plate
+	event.hide('thermal:gold_plate')
+	event.hide('immersiveengineering:plate_gold')
+	//Gold rod
+	event.hide('immersiveposts:stick_gold')
+
+	//Diamond dust
+	event.hide('mekanism:dust_diamond')
+	//Diamond gear
+	event.hide('titanium:diamond_gear')
+
+	//Emerald dust
+	event.hide('mekanism:dust_emerald')
+
+	//Lapis lazuli dust
+	event.hide('mekanism:dust_lapis_lazuli')
+
+	//Nether quartz dust
+	event.hide('mekanism:dust_quartz')
+	event.hide('appliedenergistics2:nether_quartz_dust')
+
+	//Copper ingot
+	event.hide('tmechworks:copper_ingot')
+	event.hide('mekanism:ingot_copper')
+	event.hide('immersiveengineering:ingot_copper')
+	event.hide('create:copper_ingot')
+	event.hide('tconstruct:copper_ingot')
+	event.hide('iceandfire:copper_ingot')
+	//Copper block
+	event.hide('tmechworks:copper_block')
+	event.hide('mekanism:block_copper')
+	event.hide('immersiveengineering:storage_copper')
+	event.hide('create:copper_block')
+	event.hide('tconstruct:copper_block')
+	event.hide('iceandfire:copper_block')
+	//Copper nugget
+	event.hide('tmechworks:copper_nugget')
+	event.hide('mekanism:nugget_copper')
+	event.hide('immersiveengineering:nugget_copper')
+	event.hide('create:copper_nugget')
+	event.hide('tconstruct:copper_nugget')
+	event.hide('iceandfire:copper_nugget')
+	//Copper dust
+	event.hide('immersiveengineering:dust_copper')
+	event.hide('mekanism:dust_copper')
+	event.hide('pedestals:dustcopper')
+	//Copper plate
+	event.hide('create:copper_sheet')
+	event.hide('immersiveengineering:plate_copper')
+	//Copper ore
+	event.hide('immersiveengineering:ore_copper')
+	event.hide('mekanism:copper_ore')
+	event.hide('create:copper_ore')
+	event.hide('thermal:copper_ore')
+	event.hide('tmechworks:copper_ore')
+	event.hide('tconstruct:copper_ore')
+	event.hide('iceandfire:copper_ore')
+
+	//Aluminum ingot
+	event.hide('tmechworks:aluminum_ingot')
+	event.hide('immersiveengineering:ingot_aluminium')
+	//Aluminum block
+	event.hide('tmechworks:aluminum_block')
+	event.hide('immersiveengineering:storage_aluminium')
+	//Aluminum nugget
+	event.hide('tmechworks:aluminum_nugget')
+	event.hide('immersiveengineering:nugget_aluminium')
+	//Aluminum dust
+	event.hide('pedestals:dustaluminum')
+	event.hide('immersiveengineering:dust_aluminium')
+	//Aluminum ore
+	event.hide('tmechworks:aluminum_ore')
+	event.hide('immersiveengineering:ore_aluminum')
+
+	//Silver ingot
+	event.hide('immersiveengineering:ingot_silver')
+	event.hide('iceandfire:silver_ingot')
+	//Silver block
+	event.hide('immersiveengineering:storage_silver')
+	event.hide('iceandfire:silver_block')
+	//Silver nugget
+	event.hide('immersiveengineering:nugget_silver')
+	event.hide('iceandfire:silver_nugget')
+	//Silver dust
+	event.hide('immersiveengineering:dust_silver')
+	event.hide('pedestals:dustsilver')
+	//Silver plate
+	event.hide('immersiveengineering:plate_silver')
+	//Silver ore
+	event.hide('immersiveengineering:ore_silver')
+	event.hide('iceandfire:silver_ore')
+
+	//Lead ingot
+	event.hide('mekanism:ingot_lead')
+	event.hide('immersiveengineering:ingot_lead')
+	event.hide('eidolon:lead_ingot')
+	//Lead block
+	event.hide('mekanism:block_lead')
+	event.hide('immersiveengineering:storage_lead')
+	event.hide('eidolon:lead_block')
+	//Lead nugget
+	event.hide('mekanism:nugget_lead')
+	event.hide('immersiveengineering:nugget_lead')
+	event.hide('eidolon:lead_nugget')
+	//Lead dust
+	event.hide('mekanism:dust_lead')
+	event.hide('immersiveengineering:dust_lead')
+	event.hide('eidolon:lead_dust')
+	event.hide('pedestals:dustlead')
+	//Lead plate
+	event.hide('immersiveengineering:plate_lead')
+	//Lead ore
+	event.hide('immersiveengineering:ore_lead')
+	event.hide('mekanism:lead_ore')
+	event.hide('eidolon:lead_ore')
+
+	//Nickel ingot
+	event.hide('silents_mechanisms:nickel_ingot')
+	event.hide('thermal:nickel_ingot')
+	event.hide('immersiveengineering:ingot_nickel')
+	event.hide('geolosys:nickel_ingot')
+	//Nickel block
+	event.hide('silents_mechanisms:nickel_block')
+	event.hide('thermal:nickel_block')
+	event.hide('immersiveengineering:storage_nickel')
+	//Nickel nugget
+	event.hide('silents_mechanisms:nickel_nugget')
+	event.hide('thermal:nickel_nugget')
+	event.hide('immersiveengineering:nugget_nickel')
+	event.hide('geolosys:nickel_nugget')
+	//Nickel dust
+	event.hide('silents_mechanisms:nickel_dust')
+	event.hide('thermal:nickel_dust')
+	event.hide('immersiveengineering:dust_nickel')
+	event.hide('pedestals:dustnickel')
+	//Nickel gear
+	event.hide('thermal:nickel_gear')
+	//Nickel plate
+	event.hide('thermal:nickel_plate')
+	event.hide('immersiveengineering:plate_nickel')
+	//Nickel rod
+	event.hide('immersiveposts:stick_nickel')
+	//Nickel ore
+	event.hide('immersiveengineering:ore_nickel')
+	event.hide('thermal:nickel_ore')
+	event.hide('silents_mechanisms:nickel_ore')
+	event.hide('geolosys:nickel_cluster')
+
+	//Uranium ingot
+	event.hide('silents_mechanisms:uranium_ingot')
+	event.hide('mekanism:ingot_uranium')
+	event.hide('immersiveengineering:ingot_uranium')
+	//Uranium block
+	event.hide('silents_mechanisms:uranium_block')
+	event.hide('mekanism:block_uranium')
+	event.hide('immersiveengineering:storage_uranium')
+	//Uranium nugget
+	event.hide('silents_mechanisms:uranium_nugget')
+	event.hide('mekanism:nugget_uranium')
+	event.hide('immersiveengineering:nugget_uranium')
+	//Uranium dust
+	event.hide('silents_mechanisms:uranium_dust')
+	event.hide('mekanism:dust_uranium')
+	event.hide('immersiveengineering:dust_uranium')
+	event.hide('pedestals:dusturanium')
+	//Uranium plate
+	event.hide('immersiveengineering:plate_uranium')
+	//Uranium post
+	event.hide('immersiveposts:stick_uranium')
+	//Uranium ore
+	event.hide('immersiveengineering:ore_uranium')
+	event.hide('mekanism:uranium_ore')
+	event.hide('silents_mechanisms:uranium')
+	event.hide('geolosys:uranium_cluster')
+	event.hide('geolosys:yellorium_cluster')
+
+	//Tin ingot
+	event.hide('silents_mechanisms:tin_ingot')
+	event.hide('mekanism:ingot_tin')
+	event.hide('mysticalworld:tin_ingot')
+	event.hide('thermal:tin_ingot')
+	event.hide('geolosys:tin_ingot')
+	event.hide('projectred-core:tin_ingot')
+	//Tin block
+	event.hide('silents_mechanisms:tin_block')
+	event.hide('mekanism:block_tin')
+	event.hide('mysticalworld:tin_block')
+	event.hide('thermal:tin_block')
+	//Tin nugget
+	event.hide('silents_mechanisms:tin_nugget')
+	event.hide('mekanism:nugget_tin')
+	event.hide('mysticalworld:tin_nugget')
+	event.hide('thermal:tin_nugget')
+	event.hide('geolosys:tin_nugget')
+	//Tin dust
+	event.hide('silents_mechanisms:tin_dust')
+	event.hide('mekanism:dust_tin')
+	event.hide('mysticalworld:tin_dust')
+	event.hide('thermal:tin_dust')
+	event.hide('pedestals:dusttin')
+	//Tin gear
+	event.hide('thermal:tin_gear')
+	//Tin plate
+	event.hide('thermal:tin_plate')
+	//Tin ore
+	event.hide('mekanism:tin_ore')
+	event.hide('thermal:tin_ore')
+	event.hide('mysticalworld:tin_ore')
+	event.hide('silents_mechanisms:tin_ore')
+	event.hide('geolosys:tin_cluster')
+	event.hide('projectred-exploration:tin_ore')
+
+	//Bitumen gem
+	event.hide('immersivepetroleum:bitumen')
+	event.hide('thermal:bitumen')
+	//Bitumen block
+	event.hide('thermal:bitumen_block')
+
+	//Sulfur gem
+	event.hide('thermal:sulfur')
+	//Sulfur block
+	event.hide('thermal:sulfur_block')
+	//Sulfur dust
+	event.hide('thermal:sulfur_dust')
+	event.hide('immersiveengineering:dust_sulfur')
+	event.hide('mekanism:dust_sulfur')
+	event.hide('eidolon:sulfur')
+
+	//Bronze ingot
+	event.hide('mekanism:ingot_bronze')
+	//Bronze block
+	event.hide('mekanism:block_bronze')
+	//Bronze nugget
+	event.hide('mekanism:nugget_bronze')
+	//Bronze dust
+	event.hide('mekanism:dust_bronze')
+
+	//Electrum ingot
+	event.hide('immersiveengineering:ingot_electrum')
+	//Electrum block
+	event.hide('immersiveengineering:storage_electrum')
+	//Electrum nugget
+	event.hide('immersiveengineering:nugget_electrum')
+	//Electrum dust
+	event.hide('immersiveengineering:dust_electrum')
+	event.hide('immersiveengineering:plate_electrum')
+	//CONSTANTAN
+	//Constantan ingot
+	event.hide('thermal:constantan_ingot')
+	event.hide('immersiveengineering:ingot_constantan')
+	//Constantan block
+	event.hide('thermal:constantan_block')
+	event.hide('immersiveengineering:storage_constantan')
+	//Constantan nugget
+	event.hide('thermal:constantan_nugget')
+	event.hide('immersiveengineering:nugget_constantan')
+	//Constantan dust
+	event.hide('immersiveengineering:dust_constantan')
+	//Constantan gear
+	event.hide('thermal:constantan_gear')
+	//Constantan plate
+	event.hide('thermal:constantan_plate')
+	event.hide('immersiveengineering:plate_constantan')
+	//Constantan rod
+	event.hide('immersiveposts:stick_constantan')
+	//
+	//STEEL
+	//Steel ingot
+	event.hide('silents_mechanisms:steel_ingot')
+	event.hide('mekanism:ingot_steel')
+	event.hide('assemblylinemachines:steel_ingot')
+	event.hide('immersiveengineering:ingot_steel')
+	//Steel block
+	event.hide('silents_mechanisms:steel_block')
+	event.hide('mekanism:block_steel')
+	event.hide('assemblylinemachines:steel_block')
+	event.hide('immersiveengineering:storage_steel')
+	//Steel nugget
+	event.hide('silents_mechanisms:steel_nugget')
+	event.hide('mekanism:nugget_steel')
+	event.hide('assemblylinemachines:steel_nugget')
+	event.hide('immersiveengineering:nugget_steel')
+	//Steel dust
+	event.hide('silents_mechanisms:steel_dust')
+	event.hide('mekanism:dust_steel')
+	event.hide('immersiveengineering:dust_steel')
+	//Steel plate
+	event.hide('immersiveengineering:plate_steel')
+	//Steel rod
+	event.hide('immersiveengineering:stick_steel')
+	//
+	//INVAR
+	//Invar ingot
+	event.hide('silents_mechanisms:invar_ingot')
+	event.hide('thermal:invar_ingot')
+	//Invar block
+	event.hide('silents_mechanisms:invar_block')
+	event.hide('thermal:invar_block')
+	//Invar nugget
+	event.hide('silents_mechanisms:invar_nugget')
+	event.hide('thermal:invar_nugget')
+	//Invar dust
+	event.hide('silents_mechanisms:invar_dust')
+	//Invar gear
+	event.hide('thermal:invar_gear')
+	//Invar plate
+	event.hide('thermal:invar_plate')
+	//
+	//LUMIUM
+	//Lumium ingot
+	event.hide('silents_mechanisms:lumium_ingot')
+	event.hide('thermal:lumium_ingot')
+	//Lumium block
+	event.hide('silents_mechanisms:lumium_block')
+	event.hide('thermal:lumium_block')
+	//Lumium nugget
+	event.hide('silents_mechanisms:lumium_nugget')
+	event.hide('thermal:lumium_nugget')
+	//Lumium dust
+	event.hide('silents_mechanisms:lumium_dust')
+	//Lumium gear
+	event.hide('thermal:lumium_gear')
+	//Lumium plate
+	event.hide('thermal:lumium_plate')
+	//
+	//SIGNALUM
+	//Signalum ingot
+	event.hide('silents_mechanisms:signalum_ingot')
+	event.hide('thermal:signalum_ingot')
+	//Signalum block
+	event.hide('silents_mechanisms:signalum_block')
+	event.hide('thermal:signalum_block')
+	//Signalum nugget
+	event.hide('silents_mechanisms:signalum_nugget')
+	event.hide('thermal:signalum_nugget')
+	//Signalum dust
+	event.hide('silents_mechanisms:signalum_dust')
+	//Signalum gear
+	event.hide('thermal:signalum_gear')
+	//Signalum plate
+	event.hide('thermal:signalum_plate')
+	//
+	//ENDERIUM
+	//Enderium ingot
+	event.hide('silents_mechanisms:enderium_ingot')
+	event.hide('thermal:enderium_ingot')
+	//Enderium block
+	event.hide('silents_mechanisms:enderium_block')
+	event.hide('thermal:enderium_block')
+	//Enderium nugget
+	event.hide('silents_mechanisms:enderium_nugget')
+	event.hide('thermal:enderium_nugget')
+	//Enderium dust
+	event.hide('silents_mechanisms:enderium_dust')
+	//Enderium gear
+	event.hide('thermal:enderium_gear')
+	//Enderium plate
+	event.hide('thermal:enderium_plate')
+	//
+	//SILICON
+	//Silicon gem.
+	event.hide('appliedenergistics2:silicon')
+	event.hide('tinyredstone:silicon')
+	event.hide('refinedstorage:silicon')
+	//event.hide('emendatusenigmatica:silicon')
+	//
+	//COAL COKE
+	//Coal coke gem
+	event.hide('immersiveengineering:coal_coke')
+	event.hide('thermal:coal_coke')
+	//Coal coke block
+	event.hide('immersiveengineering:coke')
+	event.hide('thermal:coal_coke_block')
+	//Coal coke dust
+	event.hide('immersiveengineering:dust_coke')
+	//
+	//CHARCOAL
+	//Charcoal dust
+	event.hide('mekanism:dust_charcoal')
+	event.hide('assemblylinemachines:ground_charcoal')
+	//
+	//OTHER
+	event.hide('minecraft:redstone_ore')
+	event.hide('geolosys:ancient_debris_cluster')
+	//
+	//fin
+	//
+	})
